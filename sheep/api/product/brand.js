584b935ff23745b744bc9d8aba7081530615a296
Ling <jia612516@lingjia.fun>
2024-04-07 20:10:28 +0800

    分类页面修改

--- sheep/api/product/brand.js
+++ sheep/api/product/brand.js
@@ -2,11 +2,11 @@ import request from '@/sheep/request';
 
 const BrandApi = {
 	// 根据分类id查询品牌，不传id查询所有
-	getBrandListByCategoryId: (id) => {
+	getBrandListByCategoryId: (categoryId) => {
 	  return request({
 	    url: '/product/brand/list',
 	    method: 'GET',
-	    params: { id },
+	    params: { categoryId },
 	  });
 	},
 }
